7f7c799f7ceba2c70af133b8fdf4a5f67a93f70a
ttenso <tenso@tlu.ee>
2024-03-28 00:11:20 +0200

    DB

--- DB/config.php
+++ DB/config.php
@@ -1,14 +1,60 @@
 <?php
-use MongoDB\Driver\ServerApi;
-$uri = 'mongodb+srv://Tenso:Dti2023@cluster0.v10lvza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-// Set the version of the Stable API on the client
-$apiVersion = new ServerApi(ServerApi::V1);
-// Create a new client and connect to the server
-$client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
-try {
-    // Send a ping to confirm a successful connection
-    $client->selectDatabase('admin')->command(['ping' => 1]);
-    echo "Pinged your deployment. You successfully connected to MongoDB!\n";
-} catch (Exception $e) {
-    printf($e->getMessage());
-} 
+
+//this pulls the MongoDB driver from vendor folder
+require_once __DIR__ . '/vendor/autoload.php';
+
+//connect to MongoDB Database
+
+// muuda strinigs pw ja user ära
+$databaseConnection = new MongoDB\Client(
+    'mongodb+srv://SIIAUSER:SIIAPW@cluster0.v10lvza.mongodb.net/?tls=true&tlsCAFile=C%3A%5Cxampp%5Capache%5Cbin%5Ccurl-ca-bundle.crt'
+);
+
+//connecting to specific database in mongoDB
+$myDatabase = $databaseConnection->DTI_Database;
+
+//connecting to our mongoDB Collections
+$userCollection = $myDatabase->users;
+
+// if($userCollection){
+// 	echo "Collection ".$userCollection." Connected";
+// }
+// else{
+// 	echo "Failed to connect to Database/Collection";
+// }
+
+if (isset($_POST['signup'])) {
+
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $phoneNo = $_POST['phoneNo'];
+    $password = sha1($_POST['password']);
+}
+
+$data = array(
+    "Firstname" => $fname,
+    "Lastname" => $lname,
+    "Email" => $email,
+    "Phone Number" => $phoneNo,
+    "Password" => $password
+);
+
+//insert into MongoDB Users Collection
+$insert = $userCollection->insertOne($data);
+
+if ($insert) {
+    ?>
+    <center>
+        <h4 style="color: green;">Successfully Registered</h4>
+    </center>
+    <center><a href="../index.php">Login</a></center>
+    <?php
+} else {
+    ?>
+    <center>
+        <h4 style="color: red;">Registration Failed</h4>
+    </center>
+    <center><a href="inseert.php">Try Again</a></center>
+    <?php
+}
