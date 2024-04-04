1fd694a7b2316fcf2974bf1ca7ce99bf0f18b0a3
Henry Naptal <henrynaptal@users.noreply.github.com>
2024-04-04 16:27:58 +0300

    signup kood

--- signup.php
+++ signup.php
@@ -1,3 +1,39 @@
+<?php
+
+require 'vendor/autoload.php'; // Laadige Composeri loodud autoload fail
+
+use MongoDB\Client as MongoClient;
+
+// ühendusstring
+$connectionString = "mongodb://<kasutajanimi>:<parool>@<andmebaasi_host>:<port>/<andmebaasi_nimi>";
+
+// loon MongoDB kliendi
+$client = new MongoClient($connectionString);
+
+// valin andmebaasi
+$database = $client->selectDatabase('<andmebaasi_nimi>');
+
+// valin kollektsiooni (tabel), kus kasutajate andmed on salvestatud
+$collection = $database->selectCollection('<kasutajate_kollektsiooni_nimi>');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $eesnimi = $_POST['fname'];
+    $perekonnanimi = $_POST['lname'];
+    $email = $_POST['email'];
+    $parool = $_POST['password'];
+
+    $collection->insertOne([
+        'eesnimi' => $eesnimi,
+        'perekonnanimi' => $perekonnanimi,
+        'email' => $email,
+        'parool' => $parool
+    ]);
+
+    header('Location: login.php');
+    exit;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +42,7 @@
     <title>DTI tudengite portaal</title>
 </head>
 <body>
-    <form action="" method="POST">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
         <input type="text" placeholder="Eesnimi" name="fname" id="fname" required=""/>
         <br>
         <input type="text" placeholder="Perekonnanimi" name="lname" id="lname" required=""/>
