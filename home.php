f6e9948372012309e8a4cf3798249d990132d259
lxkyo <lxkyo@tlu.ee>
2024-04-07 22:24:58 +0300

    kodulehele pic + teksti

--- home.php
+++ home.php
@@ -3,66 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css">
     <title>DTI tudengite portaal</title>
-
-    <style>
-
-        body {
-            margin: 0;
-        }
-
-        .navbar {
-        }
-
-        .header {
-            margin-top: 2rem;
-            padding-left: 5rem;
-            padding-right: 5rem;
-            align-items: flex-end; /*et nav osa oleks divi all osas, mitte yleval*/
-        }
-
-        ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-            background-color: white; 
-        }
-
-        li {
-            float: right;
-        }
-
-        li a {
-            display: block;
-            color: #3c4445;
-            font-style: italic;
-            font-family: "Times New Roman", serif;
-            text-transform: uppercase;
-            text-align: center;
-            padding: 2rem 2rem;
-            text-decoration: none;
-            z-index: 2;
-        }
-
-        li a:hover {
-            color: #6bcaba;
-        }
-
-        .sidebar {
-            position: fixed;
-            top: 0;
-            left: 0;
-            bottom: 0;
-            background: #6bcaba;
-            display: flex;
-            z-index: 0;
-            width: 15%;
-            height: 100%;
-            margin-top: 10%;
-        }
-
-    </style>
 </head>
 
 <body>
@@ -85,10 +27,15 @@
     </div>
 
     <div class="dp">
-        <img src="pics/dp2.jpg" alt="students">
-        <a href="#" class="dp"><img src="tudengiteportaal\pics\dp2.jpg" alt="students"></a>
-
+        <img src="pics/dp.jpg" alt="students">
     </div>
+
+    <div class="welcome">
+        <h1>Tere, Heli!</h1>
+        <h2>Kuidas Sa end täna tunned?</h2>
+    </div>
+
+
 
     <div class="sidebar"></div> 
 
--- login.php
+++ login.php
@@ -4,16 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DTI tudengite portaal</title>
+    <link rel="stylesheet" href="login.css">
 </head>
-<body>
-    <form action="" method="POST">
-        <input type="text" placeholder="Email" name="email" id="email" required=""/>
-        <br>
-        <input type="text" placeholder="Parool" name="password" id="password" required=""/>
-        <br>
-        <input type="submit" name="login" id="login" value="Logi sisse"/>
-    </form>
 
-    <a href="signup.php">Kas sul pole kontot? Registreeru</a>
+<body>
+    <div class="wrapper">
+
+        <div class="login_container">
+            <div class="login-header">
+                <span>Tallinna Ülikooli Digitehnoloogiate instituudi tudengiportaal<span>
+                </div>
+
+                <p>Logi sisse<p>
+                <br>
+
+                <form action="" method="POST">
+                    <div class="input_box">
+                        <input type="text" placeholder="E-post" name="email" id="email" class="input-field" required=""/>
+                    </div>
+                    <br>
+                    <div class="input_box">
+                        <input type="text" placeholder="Parool" name="password" id="password" class="input-field" required=""/>
+                    </div>
+                    <br>
+                    <div class="input_box">
+                        <input type="submit" name="login" id="login" class="input-submit" value="Logi sisse"/>
+                    </div>
+                </form>
+
+                <a href="signup.php">Ei ole kontot? Vajuta siia!</a>
+            </div>
+        </div>
+    </div>
+
+
+    
+
 </body>
 </html>
